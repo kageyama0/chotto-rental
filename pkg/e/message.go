ed40ef9f9c11ea085b43259e4383266f9527835b
yuki kageyama <kageyamaforwork@gmail.com>
2024-12-13 04:18:49 +0900

    エラーメッセージにステータスコードはやっぱりいらない気がする

--- pkg/e/message.go
+++ pkg/e/message.go
@@ -2,36 +2,36 @@ package e
 
 var MsgFlags = map[int]string{
 	// 200
-	OK:                              "200.OK",
-	CREATED:                         "201.Created",
-	NO_CONTENT:                      "204.No Content",
+	OK:                              "OK",
+	CREATED:                         "Created",
+	NO_CONTENT:                      "No Content",
 
 	// 400系エラーメッセージ
-	BAD_REQUEST:                     "400.リクエストが不正です",
-	JSON_PARSE_ERROR:                "400.リクエストの形式が不正です",
-	INVALID_ID:                      "400.無効なIDを使用しています",
-	CASE_NOT_OPEN:                   "400.この案件は募集を終了しています",
-	ALREADY_APPLIED:                 "400.既にこの案件に応募しています",
-	OVER_CONFIRMATION_DEADLINE:      "400.確認期限が過ぎています",
+	BAD_REQUEST:                     "リクエストが不正です",
+	JSON_PARSE_ERROR:                "リクエストの形式が不正です",
+	INVALID_ID:                      "無効なIDを使用しています",
+	CASE_NOT_OPEN:                   "この案件は募集を終了しています",
+	ALREADY_APPLIED:                 "既にこの案件に応募しています",
+	OVER_CONFIRMATION_DEADLINE:      "確認期限が過ぎています",
 
 	// 401系エラーメッセージ
-	UNAUTHORIZED:                    "401.認証エラー",
-	INVALID_TOKEN:                   "401.無効なトークンです",
-	INVALID_TOKEN_FORMAT:            "401.無効なトークンフォーマットです",
-	AUTH_REQUIRED:                   "401.認証情報が必要です",
+	UNAUTHORIZED:                    "認証エラー",
+	INVALID_TOKEN:                   "無効なトークンです",
+	INVALID_TOKEN_FORMAT:            "無効なトークンフォーマットです",
+	AUTH_REQUIRED:                   "認証情報が必要です",
 
 	// 403系エラーメッセージ
-	FORBIDDEN:                       "403.この操作を行う権限がありません",
-	FORBIDDEN_UPDATE_APPLICATION:    "403.この応募のステータスを更新する権限がありません",
-	FORBIDDEN_DELETE_CASE:           "403.この案件を削除する権限がありません",
+	FORBIDDEN:                       "この操作を行う権限がありません",
+	FORBIDDEN_UPDATE_APPLICATION:    "この応募のステータスを更新する権限がありません",
+	FORBIDDEN_DELETE_CASE:           "この案件を削除する権限がありません",
 
 
 	// 404系エラーメッセージ
-	NOT_FOUND:                       "404.Not Found",
-	NOT_FOUND_USER:                  "404.ユーザーが見つかりません",
-	NOT_FOUND_CASE:                  "404.案件が見つかりません",
-	NOT_FOUND_APPLICATION:           "404.応募が見つかりません",
-	NOT_FOUND_REVIEW:                "404.レビューが見つかりません",
+	NOT_FOUND:                       "Not Found",
+	NOT_FOUND_USER:                  "ユーザーが見つかりません",
+	NOT_FOUND_CASE:                  "案件が見つかりません",
+	NOT_FOUND_APPLICATION:           "応募が見つかりません",
+	NOT_FOUND_REVIEW:                "レビューが見つかりません",
 
 
 	// 500系エラーメッセージ
